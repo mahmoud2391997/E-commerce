14d2390aabbc0851a51a6bf2acae4df128bad36b
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-03-08 21:18:41 +0200

    tested

--- JS/dark_mode.js
+++ JS/dark_mode.js
@@ -1,4 +1,13 @@
 let darkness;
+let Total = document.getElementById(`total`);
+let empty = document.getElementById("empty")
+let brand = document.getElementById("brand");
+let home = document.getElementById("home");
+let products = document.getElementById("products");
+let featured = document.getElementById("featured");
+let category = document.getElementById("category");
+let asc = document.getElementById("ascending");
+let desc = document.getElementById("descending");
 let dark = document.getElementById("dark");
 let light = document.getElementById("light");
 let darkMode = document.getElementById("dark_mode");
@@ -14,39 +23,75 @@ dark.addEventListener("click",()=>{
     lm.style.display = "block"
     lightMode.style.display = "block"
     document.body.classList.toggle("dark-mode");
+    brand.classList.toggle("dark-mode-text");
+    home.classList.toggle("dark-mode-text");
+    products.classList.toggle("dark-mode-text");
+    featured?.classList.toggle("dark-mode-text");
+    category?.classList.toggle("dark-mode-text");
+    asc?.classList.toggle("dark-mode-text");
+    desc?.classList.toggle("dark-mode-text");
+    empty?.classList.toggle("dark-mode-text");
+    Total?.classList.toggle("dark-mode-text");
 });
 darkMode.addEventListener("click",()=>{
     darkness = true;
     localStorage.setItem("dark_mode",JSON.stringify(darkness));
     console.log(JSON.parse(localStorage.getItem("dark_mode")));
-
+    
     dm.style.display = "none"
     darkMode.style.display = "none"
     lm.style.display = "block"
     lightMode.style.display = "block"
     document.body.classList.toggle("dark-mode");
+    brand.classList.toggle("dark-mode-text");
+    home.classList.toggle("dark-mode-text");
+    products.classList.toggle("dark-mode-text");
+    featured?.classList.toggle("dark-mode-text");
+    category?.classList.toggle("dark-mode-text");
+    asc?.classList.toggle("dark-mode-text");
+    desc?.classList.toggle("dark-mode-text");
+    empty?.classList.toggle("dark-mode-text");
+    Total?.classList.toggle("dark-mode-text");
 });
 light.addEventListener("click",()=>{
     darkness = false;
     localStorage.setItem("dark_mode",JSON.stringify(darkness));
     console.log(JSON.parse(localStorage.getItem("dark_mode")));
-
+    
     dm.style.display = "block"
     darkMode.style.display = "block"
     lm.style.display = "none"
     lightMode.style.display = "none"
     document.body.classList.toggle("dark-mode"); 
+    brand.classList.toggle("dark-mode-text");
+    home.classList.toggle("dark-mode-text");
+    products.classList.toggle("dark-mode-text");
+    featured?.classList.toggle("dark-mode-text");
+    category?.classList.toggle("dark-mode-text");
+    asc?.classList.toggle("dark-mode-text");
+    desc?.classList.toggle("dark-mode-text");
+    empty?.classList.toggle("dark-mode-text");
+    Total?.classList.toggle("dark-mode-text");
 });
 lightMode.addEventListener("click",()=>{
     darkness = false;
     localStorage.setItem("dark_mode",JSON.stringify(darkness));
     console.log(JSON.parse(localStorage.getItem("dark_mode")));
-
+    
     dm.style.display = "block"
     darkMode.style.display = "block"
     lm.style.display = "none"
     lightMode.style.display = "none"
     document.body.classList.toggle("dark-mode"); 
+    brand.classList.toggle("dark-mode-text");
+    home.classList.toggle("dark-mode-text");
+    products.classList.toggle("dark-mode-text");
+    featured?.classList.toggle("dark-mode-text");
+    category?.classList.toggle("dark-mode-text");
+    asc?.classList.toggle("dark-mode-text");
+    desc?.classList.toggle("dark-mode-text");
+    empty?.classList.toggle("dark-mode-text");
+    Total?.classList.toggle("dark-mode-text");
 });
 if (JSON.parse(localStorage.getItem("dark_mode"))) {
     console.log("dark");
@@ -55,6 +100,15 @@ if (JSON.parse(localStorage.getItem("dark_mode"))) {
     lm.style.display = "block"
     lightMode.style.display = "block"
     document.body.classList.add("dark-mode"); 
+    brand.classList.toggle("dark-mode-text");
+    home.classList.toggle("dark-mode-text");
+    products.classList.toggle("dark-mode-text");
+    featured?.classList.toggle("dark-mode-text");
+    category?.classList.toggle("dark-mode-text");
+    asc?.classList.toggle("dark-mode-text");
+    desc?.classList.toggle("dark-mode-text");
+    empty?.classList.toggle("dark-mode-text");
+    Total?.classList.toggle("dark-mode-text");
 } else {
     console.log("light");
     dm.style.display = "block"
@@ -62,6 +116,6 @@ if (JSON.parse(localStorage.getItem("dark_mode"))) {
     lm.style.display = "none"
     lightMode.style.display = "none"
     document.body.classList.remove("dark-mode");
-    document.body.style.background = "#f7fafd"
+   
 
 }

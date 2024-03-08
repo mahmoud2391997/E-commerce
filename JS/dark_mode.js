3e36c4784f42f78240a9880fce98f756d4b73d3f
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-03-08 17:39:18 +0200

    done

--- JS/dark_mode.js
+++ JS/dark_mode.js
@@ -1,3 +1,4 @@
+let darkness;
 let dark = document.getElementById("dark");
 let light = document.getElementById("light");
 let darkMode = document.getElementById("dark_mode");
@@ -5,30 +6,62 @@ let lightMode = document.getElementById("light_mode");
 let dm = document.getElementById("dm");
 let lm = document.getElementById("lm");
 dark.addEventListener("click",()=>{
+    darkness = true;
+    localStorage.setItem("dark_mode",JSON.stringify(darkness));
+    console.log(JSON.parse(localStorage.getItem("dark_mode")));
     dm.style.display = "none"
     darkMode.style.display = "none"
     lm.style.display = "block"
     lightMode.style.display = "block"
-    document.body.style.backgroundColor = "#000d1a";
+    document.body.classList.toggle("dark-mode");
 });
 darkMode.addEventListener("click",()=>{
+    darkness = true;
+    localStorage.setItem("dark_mode",JSON.stringify(darkness));
+    console.log(JSON.parse(localStorage.getItem("dark_mode")));
+
     dm.style.display = "none"
     darkMode.style.display = "none"
     lm.style.display = "block"
     lightMode.style.display = "block"
-    document.body.style.backgroundColor = "#000d1a";
+    document.body.classList.toggle("dark-mode");
 });
 light.addEventListener("click",()=>{
+    darkness = false;
+    localStorage.setItem("dark_mode",JSON.stringify(darkness));
+    console.log(JSON.parse(localStorage.getItem("dark_mode")));
+
     dm.style.display = "block"
     darkMode.style.display = "block"
     lm.style.display = "none"
     lightMode.style.display = "none"
-    document.body.style.backgroundColor = "#f7fafd";
+    document.body.classList.toggle("dark-mode"); 
 });
 lightMode.addEventListener("click",()=>{
+    darkness = false;
+    localStorage.setItem("dark_mode",JSON.stringify(darkness));
+    console.log(JSON.parse(localStorage.getItem("dark_mode")));
+
     dm.style.display = "block"
     darkMode.style.display = "block"
     lm.style.display = "none"
     lightMode.style.display = "none"
-    document.body.style.backgroundColor = "#f7fafd";
+    document.body.classList.toggle("dark-mode"); 
 });
+if (JSON.parse(localStorage.getItem("dark_mode"))) {
+    console.log("dark");
+    dm.style.display = "none"
+    darkMode.style.display = "none"
+    lm.style.display = "block"
+    lightMode.style.display = "block"
+    document.body.classList.add("dark-mode"); 
+} else {
+    console.log("light");
+    dm.style.display = "block"
+    darkMode.style.display = "block"
+    lm.style.display = "none"
+    lightMode.style.display = "none"
+    document.body.classList.remove("dark-mode");
+    document.body.style.background = "#f7fafd"
+
+}
